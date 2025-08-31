e41bae5c8e240e983528c2f5c05ff0b9ccaf75c5
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-08-31 11:42:54 +0300

    Updated error message

--- src/main/java/com/example/springbootdemoproject/shared/exceptions/InvalidClientInputException.java
+++ src/main/java/com/example/springbootdemoproject/shared/exceptions/InvalidClientInputException.java
@@ -3,5 +3,10 @@ package com.example.springbootdemoproject.shared.exceptions;
 /**
  * Exception thrown when the client input is not valid
  */
-public class InvalidClientInputException extends RuntimeException {
+public class InvalidClientInputException extends ApiException {
+    private static final int STATUS = 400;
+
+    public InvalidClientInputException(ErrorMessage errorMessage) {
+        super(STATUS, errorMessage);
+    }
 }
