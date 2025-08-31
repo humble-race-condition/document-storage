fb3a3112fc396d55001cdc958f3335b192647b86
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-08-31 12:02:11 +0300

    Fixed field service exceptions

--- src/main/java/com/example/springbootdemoproject/features/fields/FieldServiceImpl.java
+++ src/main/java/com/example/springbootdemoproject/features/fields/FieldServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.springbootdemoproject.features.fields;
 
 import com.example.springbootdemoproject.entities.DataRecord;
 import com.example.springbootdemoproject.entities.Field;
+import com.example.springbootdemoproject.shared.apimessages.MessageService;
 import com.example.springbootdemoproject.shared.base.models.requests.FieldInfo;
 import com.example.springbootdemoproject.features.fields.requests.RemoveFieldsRequest;
 import com.example.springbootdemoproject.features.fields.requests.UpdateFieldsRequest;
 import com.example.springbootdemoproject.shared.base.models.responses.DataRecordDetail;
 import com.example.springbootdemoproject.shared.base.models.responses.FieldDetail;
+import com.example.springbootdemoproject.shared.exceptions.ErrorMessage;
 import com.example.springbootdemoproject.shared.exceptions.InvalidClientInputException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,29 +17,30 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class FieldServiceImpl implements FieldService {
     private static final Logger logger = LoggerFactory.getLogger(FieldServiceImpl.class);
     private final FieldRepository fieldRepository;
+    private final MessageService messageService;
 
-    public FieldServiceImpl(FieldRepository fieldRepository) {
+    public FieldServiceImpl(FieldRepository fieldRepository, MessageService messageService) {
         this.fieldRepository = fieldRepository;
+        this.messageService = messageService;
     }
 
     @Override
     public DataRecordDetail updateDataRecordFields(int id, UpdateFieldsRequest request) {
         validateRequest(request);
 
-        Optional<DataRecord> dataRecordOptional = fieldRepository.findById(id);
-        if (dataRecordOptional.isEmpty()) {
-            logger.error("Data record with id \"{}\" not found for data record field update", id);
-            throw new InvalidClientInputException();
-        }
+        DataRecord dataRecord = fieldRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.error("Data record with id \"{}\" not found for data record field update", id);
+                    ErrorMessage errorMessage = messageService.getErrorMessage("features.fields.on.update.datarecord.datarecord.not.found", id);
+                    return new InvalidClientInputException(errorMessage);
+                });
 
-        DataRecord dataRecord = dataRecordOptional.get();
         List<Field> recordFields = dataRecord.getFields();
         Map<String, Field> existingFieldByName = recordFields.stream()
                 .collect(Collectors.toMap(Field::getName, x -> x));
@@ -62,15 +65,15 @@ public class FieldServiceImpl implements FieldService {
     public DataRecordDetail removeDataRecordFields(int id, RemoveFieldsRequest request) {
         validateRequest(request);
 
-        Optional<DataRecord> dataRecordOptional = fieldRepository.findById(id);
-        if (dataRecordOptional.isEmpty()) {
-            logger.error("Data record with id \"{}\" not found for data record field remove", id);
-            throw new InvalidClientInputException();
-        }
+        DataRecord dataRecord = fieldRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.error("Data record with id \"{}\" not found for data record field remove", id);
+                    ErrorMessage errorMessage = messageService.getErrorMessage("features.fields.on.remove.datarecord.datarecord.not.found", id);
+                    return new InvalidClientInputException(errorMessage);
+                });
 
         //ToDo add logic for unique field name constraints. unique index by datarecord id and field name. This way, only
         //one field of the same name can be added to a specific data record
-        DataRecord dataRecord = dataRecordOptional.get();
         for (String fieldName : request.fields()) {
             dataRecord.getFields().stream()
                     .filter(f -> f.getName().equals(fieldName))
