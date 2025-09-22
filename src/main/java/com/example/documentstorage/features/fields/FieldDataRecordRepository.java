d99a7024369ca863a81d67fffbc8db152f1540f3
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-09-23 01:26:31 +0300

    Renamed repositories

--- src/main/java/com/example/documentstorage/features/fields/FieldDataRecordRepository.java
+++ src/main/java/com/example/documentstorage/features/fields/FieldDataRecordRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FieldRepository extends JpaRepository<DataRecord, Integer> {
+public interface FieldDataRecordRepository extends JpaRepository<DataRecord, Integer> {
 }
