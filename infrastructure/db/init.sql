edf6f25d90b180f407ce40730b43909ac4695caf
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-09-22 15:18:31 +0300

    Updated connection string

--- infrastructure/db/init.sql
+++ infrastructure/db/init.sql
@@ -1,1 +1,2 @@
-CREATE DATABASE IF NOT EXISTS spring_demo;
+-- This script may not run when the executing service does not have execute rights to the file
+CREATE DATABASE IF NOT EXISTS document_storage;
