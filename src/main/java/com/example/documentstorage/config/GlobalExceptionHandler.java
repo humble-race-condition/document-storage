dd2e86d4a33900264b0231d6d2a03d9847bf75de
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-09-29 23:43:14 +0300

    WIP exception handling

--- src/main/java/com/example/documentstorage/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/documentstorage/config/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.example.documentstorage.config;
 
+import com.example.documentstorage.shared.base.apimessages.LocalizationService;
 import com.example.documentstorage.shared.base.errorresponse.ErrorCode;
 import com.example.documentstorage.shared.base.errorresponse.ErrorDetails;
 import com.example.documentstorage.shared.base.errorresponse.ErrorResponse;
 import com.example.documentstorage.shared.base.exceptions.ApiException;
+import com.example.documentstorage.shared.base.exceptions.ErrorMessage;
+import com.example.documentstorage.shared.base.problemdetail.ProblemDetailMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +25,11 @@ import java.util.Optional;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private final ProblemDetailMapper problemDetailMapper;
+
+    public GlobalExceptionHandler(ProblemDetailMapper problemDetailMapper) {
+        this.problemDetailMapper = problemDetailMapper;
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(Exception ex, HttpServletRequest request) {
@@ -30,6 +38,8 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    //ToDo maybe this should return error directly, without throwing an exception
+    //ToDo handle general exceptions
     //ToDo fix this - check if this handler is needed
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
@@ -47,11 +57,8 @@ public class GlobalExceptionHandler {
         logger.error(ex.getMessage(), ex);
 
         ProblemDetail problemDetail = ProblemDetail.forStatus(ex.getStatus());
-        problemDetail.setTitle(ex.getTitle());
-        problemDetail.setDetail(ex.getDetail());
-        problemDetail.setProperty("code", ex.getCode());
-        problemDetail.setProperty("timestamp", LocalDateTime.now());
-        problemDetail.setProperty("errors", ex.getErrors());
+        //ToDo remove errors from exception. The exception should hold only code
+        problemDetail = problemDetailMapper.fillProblemDetails(problemDetail, new ErrorMessage(ex.getTitle(), ex.getDetail(), ex.getCode()), ex.getErrors());
         return problemDetail;
     }
 }
