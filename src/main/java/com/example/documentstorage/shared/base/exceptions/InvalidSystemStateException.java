30e20ca8cc2753ce16d46a22a4d0973dbc4473e3
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-09-30 23:54:03 +0300

    Updated exceptions

--- src/main/java/com/example/documentstorage/shared/base/exceptions/InvalidSystemStateException.java
+++ src/main/java/com/example/documentstorage/shared/base/exceptions/InvalidSystemStateException.java
@@ -4,11 +4,7 @@ package com.example.documentstorage.shared.base.exceptions;
  * Exception thrown when the system is not in a valid state
  */
 public class InvalidSystemStateException extends ApiException {
-    private static final int STATUS = 500;
-
-    //ToDo this exception always accepts a message that is a general error. Instead, handle these and load the message
-    //in the controller advice. Do not pass it trough the constructor
-    public InvalidSystemStateException(ErrorMessage errorMessage, Exception innerException) {
-        super(STATUS, errorMessage, null, innerException);
+    protected InvalidSystemStateException(String messageKey) {
+        super(messageKey);
     }
 }
