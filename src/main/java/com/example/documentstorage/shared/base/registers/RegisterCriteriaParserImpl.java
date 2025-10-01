c192b6cf3b21860944c44638edc281575d3215e3
humble-race-condition <45735901+humble-race-condition@users.noreply.github.com>
2025-10-01 17:15:24 +0300

    Cleared up unused messages

--- src/main/java/com/example/documentstorage/shared/base/registers/RegisterCriteriaParserImpl.java
+++ src/main/java/com/example/documentstorage/shared/base/registers/RegisterCriteriaParserImpl.java
@@ -1,7 +1,5 @@
 package com.example.documentstorage.shared.base.registers;
 
-import com.example.documentstorage.shared.base.apimessages.LocalizationService;
-import com.example.documentstorage.shared.base.exceptions.ErrorMessage;
 import com.example.documentstorage.shared.base.exceptions.InvalidClientInputException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,10 +23,7 @@ public class RegisterCriteriaParserImpl implements RegisterCriteriaParser {
     public static final int PAGE = 0;
     public static final int SIZE = 1;
 
-    public final LocalizationService localizationService;
-
-    public RegisterCriteriaParserImpl(LocalizationService localizationService) {
-        this.localizationService = localizationService;
+    public RegisterCriteriaParserImpl() {
     }
 
     /**
@@ -105,8 +100,7 @@ public class RegisterCriteriaParserImpl implements RegisterCriteriaParser {
         for (String sort : validatedSorts) {
             if (sort == null) {
                 logger.error("The sort order is null");
-                ErrorMessage errorMessage = localizationService.getErrorMessage("features.datarecords.on.datarecord.get.all.sort.is.null");
-                throw new InvalidClientInputException(errorMessage);
+                throw new InvalidClientInputException("features.datarecords.on.datarecord.get.all.sort.is.null");
             }
 
             String[] splitSort = sort.split(SEPARATOR);
@@ -136,15 +130,13 @@ public class RegisterCriteriaParserImpl implements RegisterCriteriaParser {
         for (String filter : validatedFilters) {
             if (filter == null) {
                 logger.error("The filter is null");
-                ErrorMessage errorMessage = localizationService.getErrorMessage("features.datarecords.on.datarecord.get.all.filter.is.null");
-                throw new InvalidClientInputException(errorMessage);
+                throw new InvalidClientInputException("features.datarecords.on.datarecord.get.all.filter.is.null");
             }
 
             String[] splitFilter = filter.split(SEPARATOR);
             if (splitFilter.length != 3) {
                 logger.error("The filter '{}' does not have a valid format", filter);
-                ErrorMessage errorMessage = localizationService.getErrorMessage("features.datarecords.on.datarecord.get.all.filter.not.valid", filter);
-                throw new InvalidClientInputException(errorMessage);
+                throw new InvalidClientInputException("features.datarecords.on.datarecord.get.all.filter.not.valid", filter);
             }
         }
 
@@ -153,7 +145,6 @@ public class RegisterCriteriaParserImpl implements RegisterCriteriaParser {
 
     private void throwSortException(String sort) {
         logger.error("The sort '{}' does not have a valid format", sort);
-        ErrorMessage errorMessage = localizationService.getErrorMessage("features.datarecords.on.datarecord.get.all.sort.not.valid", sort);
-        throw new InvalidClientInputException(errorMessage);
+        throw new InvalidClientInputException("features.datarecords.on.datarecord.get.all.sort.not.valid", sort);
     }
 }
